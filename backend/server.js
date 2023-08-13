87c4f59574690b7fd2482e86233e374c0b4b1876
ManasKapoor18 <manasmk2016@gmail.com>
2023-08-14 02:24:34 +0530

    made add, get, delete task in frontend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Task = require("./models/taskModel");
 const taskRoutes = require("./routes/taskRoute");
-
+const cors = require("cors");
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/api/v1/tasks", taskRoutes);
 
 // Routes
---
+++ frontend/src/components/TaskList.js
@@ -0,0 +1,101 @@
+import TaskForm from "./TaskForm";
+import Task from "./Task";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { URL } from "./../App";
+import loadingImg from "../assets/loader.gif";
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    completed: false,
+  });
+  const { name } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const getTasks = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(`${URL}/api/v1/tasks`);
+      setTasks(data.tasks);
+      setIsLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const createTask = async (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    if (name === "") {
+      return toast.error("Input field can't be empty");
+    }
+    try {
+      await axios.post(`${URL}/api/v1/tasks`, formData);
+      toast.success("Task added successfully");
+      setFormData({ ...formData, name: "" });
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    try {
+      await axios.delete(`${URL}/api/v1/tasks/${id}`);
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div>
+      <h2>Task Manager</h2>
+      <TaskForm
+        name={name}
+        handleInputChange={handleInputChange}
+        createTask={createTask}
+      />
+      <div className="--flex-between --pb">
+        <p>
+          <b>Total Tasks:</b> 0
+        </p>
+        <p>
+          <b>Completed Tasks:</b> 0
+        </p>
+      </div>
+      <hr />
+      {!isLoading && tasks.length === 0 ? (
+        <p className="--py">No task added. Please add a task</p>
+      ) : (
+        <>
+          {tasks.map((task, index) => {
+            return (
+              <Task
+                key={task._id}
+                task={task}
+                index={index}
+                deleteTask={deleteTask}
+              />
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
